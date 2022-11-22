c493e2766d836c150f3a99b04cb48a66582cae6a
buyong lee <dqd001@naver.com>
2022-11-22 22:50:47 +0900

    푸시업, 플랭크 타이머 기능 추가(수정 필요)

--- pushup_plank_Project/app/src/main/java/com/example/pushup_plank_project/PlankSettings.java
+++ pushup_plank_Project/app/src/main/java/com/example/pushup_plank_project/PlankSettings.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -42,19 +45,25 @@ public class PlankSettings extends AppCompatActivity implements View.OnClickList
         plankNextBtn = (Button) findViewById(R.id.plankNextBtn);
         plankNextBtn.setOnClickListener(this);
 
-        //타이머- 1초씩 증가할때마다 좌측 상단 텍스트뷰에 숫자가 늘어나고, 5초 되면 초기화
-        //굽힌거(3회) 안굽힌거(3회)로 5초씩 총 6번 반복(반복할때마다 배,가슴 편차 하나씩 입력받아 출력됨)
+
         //타이머 3번 끝나고 text(Text6st != null)이고 if(편차 < 5) 해당 페이지 재실행, (편차 > 5)라면 서버에 데이터 전달 후 MainActivity로 넘어감
-        //현재 작동 안 함
-        Timer timer = new Timer();
 
-        TimerTask timerTask = new TimerTask() {
-            @Override
-            public void run() {
-
+        //타이머- 1초씩 증가할때마다 좌측 상단 텍스트뷰에 숫자가 늘어나고, 5초 되면 초기화
+        //총 9번 타이머 진행(굽힌거(3회) 안굽힌거(3회)로 5초씩 총 9번 반복)-반복할때마다 배,가슴 편차 하나씩 데이터 받아 출력
+        new CountDownTimer(5000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                // 두 자리 숫자로만 형식을 지정하는 데 사용
+                NumberFormat f = new DecimalFormat("00");
+                long sec = (millisUntilFinished / 1000) % 60;
+                timerText.setText(f.format(sec)+"초");
             }
-        };
-        timer.schedule(timerTask, 0, 5000);
+            // 작업이 끝나면 00초 인쇄
+            public void onFinish() {
+                timerText.setText("00초");
+                plankBellyTest1.setText("5");    //편차 넣기
+            }
+        }.start();
+
 
     }
 
